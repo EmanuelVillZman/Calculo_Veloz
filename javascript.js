d682a25d52f2af72ea0971b0b31babe7c7ab3ad8
Emanuel_Villalobos_Guzman <emanuelvillzman@gmail.com>
2021-02-10 00:05:46 -0400

    Primera prueba funcionando

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+function principal(){
+   var valor="12355555555555555555555555"
+   document.getElementById("ecuacion").value=valor
+   document.getElementById("resultado").value="RESULTADO"  
+   //var a=Math.floor(Math.random()*100)
+   //var element= document.getElementById("parrafo")
+   //element.id=a
+}
+function calculoValor(){
+}
